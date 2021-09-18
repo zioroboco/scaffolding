9e2a2deb855edc2a2a7148a10ac0284d88a38e89
Louis Quinnell <_@ziorobo.co>
2021-09-19 02:39:17 +1000

    Implement a basic generator-generator generator

--- packages/generator/src/index.ts
+++ packages/generator/src/index.ts
@@ -6,11 +6,26 @@ export default class extends Generator {
     this.sourceRoot(resolve(__dirname, "../templates"))
   }
 
+  files = [
+    "jest.config.js",
+    "src/index.ts",
+    "templates/dummyfile.txt.ejs",
+    "tsconfig.json",
+  ]
+
+  templates = ["package.json.ejs", "src/index.spec.ts.ejs"]
+
   writing() {
-    this.renderTemplate(
-      this.templatePath("dummyfile.txt.ejs"),
-      this.destinationPath("dummyfile.txt"),
-      { name: "dummyfile" }
-    )
+    this.templates.forEach(file => {
+      this.fs.copyTpl(
+        this.templatePath(file),
+        this.destinationPath(file.replace(".ejs", "")),
+        { name: "example" }
+      )
+    })
+
+    this.files.forEach(file => {
+      this.fs.copy(this.templatePath(file), this.destinationPath(file))
+    })
   }
 }
